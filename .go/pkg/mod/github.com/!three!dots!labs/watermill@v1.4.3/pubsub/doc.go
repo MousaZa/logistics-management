c75d999dbab3fb1155184e4342b31b5d34ae2871
MousaZa <mous.zeydan@gmail.com>
2026-02-24 00:36:58 +0300

    Removed trach .go .go-cache

--- .go/pkg/mod/github.com/!three!dots!labs/watermill@v1.4.3/pubsub/doc.go
+++
@@ -1,6 +0,0 @@
-// Infrastructure directory contains Pub/Subs implementations.
-//
-// Detailed Pub/Subs docs: https://watermill.io/pubsubs/
-// Getting started guide: https://watermill.io/docs/getting-started/
-
-package pubsub
